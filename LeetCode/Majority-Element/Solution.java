9f96117583ce710467e49f4d879bd8dbd1478a3b
MOHAK KUMAR <mohakkumar107@gmail.com>
2026-07-14 02:52:14 +0530

    Add solution for Majority Element

---
+++ LeetCode/Majority-Element/Solution.java
@@ -0,0 +1,17 @@
+1class Solution {
+2    public int majorityElement(int[] nums) {
+3        int n=nums.length;
+4        for(int i=0;i<n;i++){
+5            int c=0;
+6            for(int j=0;j<n;j++){
+7                if(nums[i]==nums[j]){
+8                    c++;
+9                }
+10            }
+11            if(c>n/2){
+12                return nums[i];
+13            }
+14        }
+15    return 0;
+16    }
+17}
