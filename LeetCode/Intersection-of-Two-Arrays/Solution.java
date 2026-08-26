4b73731cafb655bd637d950f80558ed29967bd60
MOHAK KUMAR <mohakkumar107@gmail.com>
2026-08-26 17:00:47 +0530

    Add solution for Intersection of Two Arrays

--- LeetCode/Intersection-of-Two-Arrays/Solution.java
+++ LeetCode/Intersection-of-Two-Arrays/Solution.java
@@ -3,28 +3,25 @@
 3        int n = nums1.length;
 4        int m = nums2.length;
 5
-6        Arrays.sort(nums1);
-7        Arrays.sort(nums2);
-8
-9        HashSet<Integer> s1 = new HashSet<>();
-10        for(int n1 : nums1){
-11            s1.add(n1);
-12        }
-13        HashSet<Integer> s2 = new HashSet<>();
-14        for(int n2 : nums2){
-15            s2.add(n2);
-16        }
-17        HashSet<Integer> s3 = new HashSet<>();
-18        for(int num : nums1){
-19            if(s2.contains(num)){
-20                s3.add(num);
-21            }
-22        }
-23        int[] result = new int[s3.size()];
-24        int i = 0;
-25        for(int num : s3){
-26            result[i++] = num;
-27        }
-28        return result;
-29    }
-30}
+6        HashSet<Integer> s1 = new HashSet<>();
+7        for(int n1 : nums1){
+8            s1.add(n1);
+9        }
+10        HashSet<Integer> s2 = new HashSet<>();
+11        for(int n2 : nums2){
+12            s2.add(n2);
+13        }
+14        HashSet<Integer> s3 = new HashSet<>();
+15        for(int num : nums1){
+16            if(s2.contains(num)){
+17                s3.add(num);
+18            }
+19        }
+20        int[] result = new int[s3.size()];
+21        int i = 0;
+22        for(int num : s3){
+23            result[i++] = num;
+24        }
+25        return result;
+26    }
+27}
